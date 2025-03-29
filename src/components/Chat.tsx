2ac2e6104b8c765f2164131440f9e66c85e89ded
Garima Mahajan <garima.kapilmhjn@gmail.com>
2025-03-28 22:26:03 -0400

    changed the placeholders in chat to match the real data

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -95,7 +95,7 @@ const Chat: React.FC = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                   </span>
-                  <span className="line-clamp-2">"Is part PS11752778 compatible with my Whirlpool refrigerator?"</span>
+                  <span className="line-clamp-2">"Is part W10295370A compatible with my Whirlpool refrigerator?"</span>
                 </button>
                 
                 <button
@@ -119,7 +119,7 @@ const Chat: React.FC = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                   </span>
-                  <span className="line-clamp-2">"How do I install a water filter for model WRS325SDHZ?"</span>
+                  <span className="line-clamp-2">"How do I install a water filter for model WRS325FDAM02?"</span>
                 </button>
               </div>
             </div>
